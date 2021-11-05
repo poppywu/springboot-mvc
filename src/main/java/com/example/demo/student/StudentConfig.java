e208a493f1f27e22f9200e2190ede2d29dc86fb6
Poppy Ng <mmiswaiting@gmail.com>
2021-11-05 02:48:40 -0700

    init project

---
+++ src/main/java/com/example/demo/student/StudentConfig.java
@@ -0,0 +1,29 @@
+package com.example.demo.student;
+
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+import java.time.LocalDate;
+import java.time.Month;
+import java.util.List;
+@Configuration
+public class StudentConfig {
+    @Bean
+    CommandLineRunner commandLineRunner(StudentRepository studentRepository){
+        return args -> {
+            Student poppy=new Student(
+                    1L,
+                    "Poppy",
+                    "Poppy@gamil.com",
+                    LocalDate.of(2000, Month.JANUARY,5)
+            );
+            Student alex=new Student(
+                    "Alex",
+                    "alex@gmail.com",
+                    LocalDate.of(2004,Month.JANUARY,4)
+            );
+            studentRepository.saveAll(List.of(poppy,alex));
+        };
+    }
+}
---
+++ src/main/java/com/example/demo/student/StudentService.java
@@ -0,0 +1,49 @@
+package com.example.demo.student;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import javax.transaction.Transactional;
+import java.util.List;
+import java.util.Objects;
+import java.util.Optional;
+
+@Service
+public class StudentService {
+    private final StudentRepository studentRepository;
+    @Autowired
+    public StudentService(StudentRepository studentRepository) {
+        this.studentRepository = studentRepository;
+    }
+
+    public List<Student> getStudent(){
+        return studentRepository.findAll();
+    }
+    public void addNewStudent(Student student){
+        Optional<Student> studentByEmail= studentRepository.findStudentByEmail(student.getEmail());
+        if(studentByEmail.isPresent()){
+            throw new IllegalStateException("email has already been taken.");
+        }
+        studentRepository.save(student);
+    }
+    public void deleteStudent(Long id){
+        Boolean exists=studentRepository.existsById(id);
+        if(!exists){
+            throw new IllegalStateException("student does not exist");
+        }
+        studentRepository.deleteById(id);
+    }
+@Transactional
+    public void updateStudent(Long id, String name, String email) {
+        Student student=studentRepository.findById(id).orElseThrow(()->new IllegalStateException("student with"+id+"does not exist."));
+        if(name!=null&& name.length()>0 &&!Objects.equals(student.getEmail(),email)){
+            student.setEmail(email);
+        }
+        if(email!=null&&email.length()>0&&!Objects.equals(student.getEmail(),email)){
+            Optional<Student> studentByEmail= studentRepository.findStudentByEmail(student.getEmail());
+            if(studentByEmail.isPresent()){
+                throw new IllegalStateException("email has already been taken.");
+            }
+            student.setEmail(email);
+        }
+    }
+}
